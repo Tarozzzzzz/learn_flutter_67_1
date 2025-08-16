400344ba4be17fbb3d15093a7870b6bf2d43328e
panuvong <6749010038@phuketvc.ac.th>
2025-08-16 14:32:46 +0700

    step9listview widget

--- lib/screen/item.dart
+++ lib/screen/item.dart
@@ -8,56 +8,30 @@ class Item extends StatefulWidget {
 }
 
 class _ItemState extends State<Item> {
-  int quantity = 10;
-
-  void incrementQuantity() {
-    setState(() {
-      quantity++;
-    });
-  }
-
-  void decrementQuantity() {
-    setState(() {
-      // if (quantity > 0) {
-      //   quantity--;
-      // }
-      quantity = quantity > 0 ? quantity - 1 : 0;
-    });
-  }
+  List data = ["สมชาย", "สมหญิง", "สมศรี", "สมปอง", "สมจิตร"];
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            "Quantity = $quantity",
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,color: Colors.white),
+    return ListView.builder(
+      itemCount: data.length,
+      itemBuilder: (context, index) {
+        return Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(30),
+            color: Colors.purpleAccent,
           ),
-
-          const SizedBox(height: 20),
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: incrementQuantity,
-                child: const Text("Increment"),
-              ),
-
-              const SizedBox(height: 20),
-
-              ElevatedButton(
-                onPressed: decrementQuantity,
-                child: const Text("Decrement"),
-              ),
-            ],
+          margin: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+          padding: EdgeInsets.all(40),
+          child: Text(
+            data[index],
+            style: TextStyle(
+              fontSize: 28,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
           ),
-
-          const SizedBox(height: 20),
-        ],
-      ),
+        );
+      },
     );
   }
 }
